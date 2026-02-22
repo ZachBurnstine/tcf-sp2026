726da7a4651bf21359417b0a9a6922451584c589
Zachary Burnstine <burnstine.z@northeastern.edu>
2026-02-21 22:39:44 -0500

    add mywebsite project

---
+++ 02-frontend/hw/mywebsite/src/app/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-4xl font-bold">Zach Burnstine</h1>
+      <p className="text-lg text-gray-400 mt-4">I am a sophomore at Northeastern!</p>
+      <a 
+  href="https://www.linkedin.com/in/zachburnstine/" 
+  target="_blank" 
+  rel="noopener noreferrer"
+  className="text-blue-500 hover:underline mt-4"
+>
+  LinkedIn
+</a>
+    </main>
+  );
+}
